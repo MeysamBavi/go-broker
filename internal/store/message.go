b0aa2f30278187669f47f70560f33c910fb04317
Meysam Bavi <meybsky@gmail.com>
2023-07-14 23:04:51 +0330

    Change structure of broker
    delete subscriber store entirely
    use sync map to store messages
    fix id bug of messages with different subjects
    use goroutine and channels to listen for new messages

--- internal/store/message.go
+++ internal/store/message.go
@@ -10,6 +10,10 @@ import (
 type Message interface {
 	SaveMessage(ctx context.Context, subject string, message *broker.Message) error
 	GetMessage(ctx context.Context, subject string, id int) (*broker.Message, error)
+	// GetNextMessage blocks until the next message on the subject is available and then returns.
+	// If previousId is not nil, the next message of this id will be returned.
+	// If beforeBlockCallBack is not nil, it will be called when the goroutine gets blocked or when the function returns. It will be called exactly once
+	GetNextMessage(ctx context.Context, subject string, previousId *int, beforeBlockCallBack func()) (*broker.Message, error)
 }
 
 var (
