484fea853476ef08c92db4c81fc3f515c1f0aa03
Meysam Bavi <meybsky@gmail.com>
2023-07-24 18:46:47 +0330

    Fix network and failing issues

--- k6/test.js
+++ k6/test.js
@@ -1,7 +1,7 @@
 import grpc from 'k6/net/grpc';
 import { check } from 'k6';
 import { b64encode } from 'k6/encoding';
-import { Counter } from 'k6/metrics';
+import { Rate } from 'k6/metrics';
 
 function randomIntBetween(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -18,7 +18,7 @@ function randomString(length, charset='abcdefghijklmnopqrstuvwxyz') {
 }
 
 const failed_connect_attempts = 'failed_connect_attempts';
-const failedConnects = new Counter(failed_connect_attempts);
+const failedConnects = new Rate(failed_connect_attempts);
 
 const subjects = [
     'alpha',
@@ -33,23 +33,21 @@ const subjects = [
     'kappa',
 ];
 
+const maxTarget = 5 * 7;
+const batch = 100;
+const timeUnit = '5s';
+
 export const options = {
+    vus: maxTarget,
     scenarios: {
-        contacts: {
+        breakpoint: {
             executor: 'ramping-arrival-rate',
-
-            // Start iterations per `timeUnit`
-            startRate: 10,
-
-            // Start `startRate` iterations per minute
-            timeUnit: '1s',
-
-            preAllocatedVUs: 50,
-
-            maxVUs: 2000,
-
+            startRate: 5 * 2,
+            timeUnit: timeUnit,
+            preAllocatedVUs: Math.floor(maxTarget * 105 / 100),
             stages: [
-                {target: 10000, duration: '10m'},
+                {target: maxTarget, duration: '40s'},
+                {target: maxTarget, duration: '5s'}
             ],
         },
     },
@@ -58,12 +56,26 @@ export const options = {
             {
                 threshold: 'p(95) < 10000',
                 abortOnFail: true,
-                delayAbortEval: '10s'
+                delayAbortEval: '5s'
             }
         ],
         failed_connect_attempts: [
             {
-                threshold: 'rate < 2',
+                threshold: 'rate <= 0.05',
+                abortOnFail: true,
+                delayAbortEval: '1s'
+            }
+        ],
+        checks: [
+            {
+                threshold: 'rate >= 0.95',
+                abortOnFail: true,
+                delayAbortEval: '5s'
+            }
+        ],
+        dropped_iterations: [
+            {
+                threshold: 'rate <= 5',
                 abortOnFail: true,
                 delayAbortEval: '10s'
             }
@@ -76,36 +88,38 @@ client.load(['../api/proto'], 'broker.proto');
 
 export default () => {
     try {
-        if (!client.isConnected) {
-            client.connect('localhost:50043', {
-                plaintext: true
-            });
-        }
+        client.connect('localhost:50043', {
+            plaintext: true
+        });
+        failedConnects.add(false);
     } catch (err) {
-        failedConnects.add(1);
+        failedConnects.add(true);
+        client.close();
         throw err;
     }
 
-    const subj = randomItem(subjects) + randomString(2, '01');
-    const expirationSeconds = randomIntBetween(1, 20);
-    const message = randomString(randomIntBetween(10, 60));
+    for (let i = 0; i < batch; i++) {
+        const subj = randomItem(subjects) + randomString(4, '01');
+        const expirationSeconds = randomIntBetween(1, 20);
+        const message = randomString(randomIntBetween(10, 60));
 
-    const data = { subject: subj, body: b64encode(message), expirationSeconds: expirationSeconds };
-    const response = client.invoke('broker.Broker/Publish', data);
+        const data = {subject: subj, body: b64encode(message), expirationSeconds: expirationSeconds};
+        const response = client.invoke('broker.Broker/Publish', data);
 
-    check(response, {
-        'publish is OK': (r) => r && r.status === grpc.StatusOK,
-    });
-
-    const doFetch = randomIntBetween(0, 2) === 0
-    if (doFetch) {
-        const data2 = { subject: subj, id: response.message.id };
-        const response2 = client.invoke('broker.Broker/Fetch', data2);
-
-        check(response2, {
-            'fetch is OK': (r) => r && r.status === grpc.StatusOK,
+        check(response, {
+            'publish is OK': (r) => r && r.status === grpc.StatusOK,
         });
+
+        const doFetch = randomIntBetween(1, 2) === 0;
+        if (doFetch) {
+            const data2 = {subject: subj, id: response.message.id};
+            const response2 = client.invoke('broker.Broker/Fetch', data2);
+
+            check(response2, {
+                'fetch is OK': (r) => r && r.status === grpc.StatusOK,
+            });
+        }
     }
 
-    client.close()
+    client.close();
 };
